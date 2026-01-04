a7cfbee80ff1358e811173b77a870c0d4bf836c5
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-01-04 15:35:31 -0500

    call litPipeline() in build.js

--- build.js
+++ build.js
@@ -1,6 +1,10 @@
 import { scssPipeline } from "./scss.js";
+import { litPipeline } from "./lit.js";
 
 const OUT_DIR = "dist/";
 
 const SCSS_DIR = "scss/";
 await scssPipeline(SCSS_DIR, OUT_DIR);
+
+const LIT_DIR = "lit/";
+await litPipeline(LIT_DIR, OUT_DIR);
