30500ea6e619b68cf6c4447b07b528d6b014e41c
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-01-05 17:58:29 -0500

    add footer.wc.js

---
+++ wc/footer.wc.js
@@ -0,0 +1,18 @@
+export default class EthmarksFooter extends HTMLElement {
+  connectedCallback() {
+    const sourceLink =
+      this.getAttribute("source") ||
+      "https://github.com/ethmarks/ethmarks.github.io";
+
+    this.innerHTML = `
+      <footer>
+        <span id="source">
+            <a href="${sourceLink}" id="sourcelink" target="_blank">Website Source</a>
+        </span>
+        <span id="copyright"><a href="https://ethmarks.github.io/about/">Ethan Marks</a>, &copy;2026</span>
+        <span id="email">
+            <a href="mailto:ethmarks.dev@gmail.com" target="_blank">Contact</a>
+        </span>
+    </footer>`;
+  }
+}
