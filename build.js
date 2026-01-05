809afd502e986a630ff05e03dc4e69272c4d1436
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-01-05 16:08:02 -0500

    add font copying

--- build.js
+++ build.js
@@ -1,12 +1,21 @@
-import * as fs from "fs";
+import * as fs from "fs/promises";
+import { existsSync } from "fs";
+import * as path from "path";
 
 export const OUT_DIR = "dist/";
-fs.mkdirSync(OUT_DIR, { recursive: true });
+fs.mkdir(OUT_DIR, { recursive: true });
+
+async function copyOut(inFile) {
+  const pathParts = inFile.split(path.sep);
+  pathParts[0] = OUT_DIR;
+  const outFile = pathParts.join(path.sep).replace(/\.scss$/, ".css");
+  await fs.copyFile(inFile, outFile);
+}
 
 // ============================================================================
 // SCSS
 // ============================================================================
-
+//
 // This section processes SCSS source into CSS stylesheets
 
 import { processSCSS } from "./scss.js";
@@ -14,9 +23,26 @@ import { processSCSS } from "./scss.js";
 await processSCSS("scss/props");
 
 // ============================================================================
+// Fonts
+// ============================================================================
+//
+// This section copies WOFF2 files into the output
+
+await copyOut("fonts/Fira_Code.woff2");
+await copyOut("fonts/Kenia.woff2");
+await copyOut("fonts/Nunito-cyrillic.woff2");
+await copyOut("fonts/Nunito-cyrillic-italic.woff2");
+await copyOut("fonts/Nunito-latin.woff2");
+await copyOut("fonts/Nunito-latin-ext.woff2");
+await copyOut("fonts/Nunito-latin-ext-italic.woff2");
+await copyOut("fonts/Nunito-latin-italic.woff2");
+await copyOut("fonts/Sen-latin.woff2");
+await copyOut("fonts/Sen-latin-ext.woff2");
+
+// ============================================================================
 // Hardpoint Verification
 // ============================================================================
-
+//
 // This section verifies that all Hardpoint assets are present in the final
 // output, and fails if any are not present
 
