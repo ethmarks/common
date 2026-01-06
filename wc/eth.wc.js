bb9fccf9279b2ea12eab0e197870eb1bfd0fb9ed
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-01-05 23:42:31 -0500

    fix imports in eth.wc.js

--- wc/eth.wc.js
+++ wc/eth.wc.js
@@ -1,5 +1,5 @@
-import EthmarksHeader from "./header.wc";
-import EthmarksFooter from "./footer.wc";
+import EthmarksHeader from "./header.wc.js";
+import EthmarksFooter from "./footer.wc.js";
 
 customElements.define("eth-header", EthmarksHeader);
 customElements.define("eth-footer", EthmarksFooter);
