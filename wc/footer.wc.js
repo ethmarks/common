c06598610d315980899e60d4c59d15ceab3cb253
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-02-02 18:14:44 -0500

    remove currentYear from footer.wc.js
    I forgot to remove it when I removed the copyright

--- wc/footer.wc.js
+++ wc/footer.wc.js
@@ -2,7 +2,6 @@ export default class EthmarksFooter extends HTMLElement {
   connectedCallback() {
     const hydrate = this.hasAttribute("hydrate");
     const source = this.getAttribute("source") || "https://github.com/ethmarks";
-    const currentYear = new Date().getFullYear();
 
     const existingFooter = this.querySelector(":scope > footer");
 
